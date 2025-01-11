fcef0e34cc32a8a7c81d23ef95d6cc02f92d18fb
amazon project <keshruwalaharsh@gmail.com>
2025-01-11 10:15:22 -0500

    Save data and generate HTML for payment summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,3 +1,15 @@
+export function getDeliveryOption(deliveryOptionId) {
+  let deliveryOption;
+  
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+
+  return deliveryOption || deliveryOptions[0];
+}
+
 export const deliveryOptions = [{
   id: '1',
   deliveryDays: 7,
@@ -5,7 +17,7 @@ export const deliveryOptions = [{
 }, {
   id: '2',
   deliveryDays: 3,
-  priceCents: 499,
+  priceCents: 499
 }, {
   id: '3',
   deliveryDays: 1,
